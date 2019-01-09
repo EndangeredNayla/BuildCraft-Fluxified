3246f7bd89728b5e10fe1a85696c52d3ab7a13dc
DBotThePony <dbotthepony@yandex.ru>
2019-01-09 13:14:07 +0700

    Implement EU on FE machines, and implemnt FE on EU machines

--- src/main/java/ru/dbotthepony/mc/gtcefe/EnergyProvider.java
+++ src/main/java/ru/dbotthepony/mc/gtcefe/EnergyProvider.java
@@ -20,8 +20,12 @@
 
 package ru.dbotthepony.mc.gtcefe;
 
+import javax.annotation.Nullable;
+
+import gregtech.api.GTValues;
 import gregtech.api.capability.GregtechCapabilities;
 import gregtech.api.capability.IEnergyContainer;
+import gregtech.common.pipelike.cable.tile.CableEnergyContainer;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
@@ -30,8 +34,9 @@ import net.minecraftforge.energy.IEnergyStorage;
 
 public class EnergyProvider implements ICapabilityProvider {
 	private final ICapabilityProvider upvalue;
-	private IEnergyContainer cache;
-	private EnergyContainerWrapper container;
+	private final EnergyContainerWrapper[] facesRF = new EnergyContainerWrapper[7];
+	private GregicEnergyContainerWrapper wrapper;
+	private boolean gettingValue = false;
 
 	public EnergyProvider(ICapabilityProvider entCap) {
 		upvalue = entCap;
@@ -39,34 +44,361 @@ public class EnergyProvider implements ICapabilityProvider {
 
 	@Override
 	public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
-		return capability == CapabilityEnergy.ENERGY && upvalue.hasCapability(GregtechCapabilities.CAPABILITY_ENERGY_CONTAINER, null);
+		if (gettingValue) {
+			return false;
+		}
+
+		//if (capability != GregtechCapabilities.CAPABILITY_ENERGY_CONTAINER) {
+		if (capability != CapabilityEnergy.ENERGY && capability != GregtechCapabilities.CAPABILITY_ENERGY_CONTAINER) {
+			return false;
+		}
+
+		if (capability == CapabilityEnergy.ENERGY) {
+			int faceID = facing == null ? 6 : facing.getIndex();
+
+			if (facesRF[faceID] == null) {
+				facesRF[faceID] = new EnergyContainerWrapper(upvalue.getCapability(GregtechCapabilities.CAPABILITY_ENERGY_CONTAINER, facing), facing);
+			}
+
+			gettingValue = true;
+			boolean result = facesRF[faceID].isValid();
+			gettingValue = false;
+			return result;
+		}
+
+		if (wrapper == null) {
+			wrapper = new GregicEnergyContainerWrapper(upvalue);
+		}
+
+		gettingValue = true;
+		boolean result = wrapper.isValid(facing);
+		gettingValue = false;
+
+		return result;
 	}
 
 	@Override
 	public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
-		if (capability == CapabilityEnergy.ENERGY) {
-			IEnergyContainer getcontainer = upvalue.getCapability(GregtechCapabilities.CAPABILITY_ENERGY_CONTAINER, null);
-
-			if (getcontainer == null) {
-				return null;
-			}
-
-			if (cache != getcontainer) {
-				cache = getcontainer;
-				container = new EnergyContainerWrapper(getcontainer);
-			}
-
-			return (T) container;
+		if (gettingValue) {
+			return null;
 		}
+
+		if (!hasCapability(capability, facing)) {
+			return null;
+		}
+
+		if (capability == CapabilityEnergy.ENERGY) {
+			int faceID = facing == null ? 6 : facing.getIndex();
+
+			if (facesRF[faceID] == null) {
+				facesRF[faceID] = new EnergyContainerWrapper(upvalue.getCapability(GregtechCapabilities.CAPABILITY_ENERGY_CONTAINER, facing), facing);
+			}
+
+			gettingValue = true;
+
+			if (facesRF[faceID].isValid()) {
+				gettingValue = false;
+				return (T) facesRF[faceID];
+			}
+
+			gettingValue = false;
+
+			return null;
+		}
+
+		if (wrapper == null) {
+			wrapper = new GregicEnergyContainerWrapper(upvalue);
+		}
+
+		gettingValue = true;
+
+		if (wrapper.isValid(facing)) {
+			gettingValue = false;
+			return (T) wrapper;
+		}
+
+		gettingValue = false;
 
 		return null;
 	}
 
-	class EnergyContainerWrapper implements IEnergyStorage {
-		private IEnergyContainer container;
+	class GregicEnergyContainerWrapper implements IEnergyContainer {
+		private final ICapabilityProvider upvalue;
+		private final IEnergyStorage[] facesRF = new IEnergyStorage[7];
 
-		public EnergyContainerWrapper(IEnergyContainer container) {
+		public GregicEnergyContainerWrapper(ICapabilityProvider upvalue) {
+			this.upvalue = upvalue;
+		}
+
+		boolean isValid(EnumFacing face) {
+			if (upvalue.hasCapability(CapabilityEnergy.ENERGY, face)) {
+				return true;
+			}
+
+			if (face == null) {
+				for (EnumFacing face2 : EnumFacing.VALUES) {
+					if (upvalue.hasCapability(CapabilityEnergy.ENERGY, face2)) {
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+
+		private IEnergyStorage getStorageCap() {
+			IEnergyStorage container = def();
+
+			if (container != null && container.getMaxEnergyStored() > 0) {
+				return container;
+			}
+
+			for (EnumFacing face : EnumFacing.VALUES) {
+				container = facesRF[face.getIndex()];
+
+				if (container == null) {
+					container = upvalue.getCapability(CapabilityEnergy.ENERGY, face);
+					facesRF[face.getIndex()] = container;
+				}
+
+				if (container != null && container.getMaxEnergyStored() > 0) {
+					return container;
+				}
+			}
+
+			return container;
+		}
+
+		private IEnergyStorage getAcceptionCap() {
+			IEnergyStorage container = def();
+
+			if (container != null && container.receiveEnergy(Integer.MAX_VALUE, true) > 0) {
+				return container;
+			}
+
+			for (EnumFacing face : EnumFacing.VALUES) {
+				container = facesRF[face.getIndex()];
+
+				if (container == null) {
+					container = upvalue.getCapability(CapabilityEnergy.ENERGY, face);
+					facesRF[face.getIndex()] = container;
+				}
+
+				if (container != null && container.receiveEnergy(Integer.MAX_VALUE, true) > 0) {
+					return container;
+				}
+			}
+
+			return container;
+		}
+
+		@Override
+		public long acceptEnergyFromNetwork(EnumFacing facing, long voltage, long amperage) {
+			int faceID = facing == null ? 6 : facing.getIndex();
+			IEnergyStorage container = facesRF[faceID];
+
+			if (container == null) {
+				container = upvalue.getCapability(CapabilityEnergy.ENERGY, facing);
+				facesRF[faceID] = container;
+			}
+
+			if (container == null) {
+				return 0L;
+			}
+
+			long maximalValue = voltage * amperage * GTCEFE.RATIO;
+
+			if (maximalValue > Integer.MAX_VALUE) {
+				maximalValue = Integer.MAX_VALUE;
+			}
+
+			int receive = container.receiveEnergy((int) maximalValue, true);
+			receive -= receive % (voltage * GTCEFE.RATIO);
+
+			if (receive == 0) {
+				return 0L;
+			}
+
+			return container.receiveEnergy(receive, false) / (voltage * GTCEFE.RATIO);
+		}
+
+		@Override
+		public long changeEnergy(long delta) {
+			IEnergyStorage container = getStorageCap();
+
+			if (container == null) {
+				return 0L;
+			}
+
+			if (delta == 0L) {
+				return 0L;
+			}
+
+			if (delta < 0L) {
+				long extractValue = delta * GTCEFE.RATIO;
+
+				if (extractValue > Integer.MAX_VALUE) {
+					extractValue = Integer.MAX_VALUE;
+				}
+
+				int extract = container.extractEnergy((int) extractValue, true);
+				extract -= extract % GTCEFE.RATIO_INT;
+				return container.extractEnergy(extract, false) / GTCEFE.RATIO;
+			}
+
+			long receiveValue = delta * GTCEFE.RATIO;
+
+			if (receiveValue > Integer.MAX_VALUE) {
+				receiveValue = Integer.MAX_VALUE;
+			}
+
+			int receive = container.receiveEnergy((int) receiveValue, true);
+			receive -= receive % GTCEFE.RATIO_INT;
+			return container.receiveEnergy(receive, false) / GTCEFE.RATIO;
+		}
+
+		@Nullable
+		private IEnergyStorage def() {
+			if (facesRF[6] == null) {
+				facesRF[6] = upvalue.getCapability(CapabilityEnergy.ENERGY, null);
+			}
+
+			return facesRF[6];
+		}
+
+		@Override
+		public long getEnergyCapacity() {
+			IEnergyStorage cap = getStorageCap();
+
+			if (cap == null) {
+				return 0L;
+			}
+
+			int value = cap.getMaxEnergyStored();
+			value -= value % GTCEFE.RATIO_INT;
+			return value / GTCEFE.RATIO_INT;
+		}
+
+		@Override
+		public long getEnergyStored() {
+			IEnergyStorage cap = getStorageCap();
+
+			if (cap == null) {
+				return 0L;
+			}
+
+			int value = cap.getEnergyStored();
+			value -= value % GTCEFE.RATIO_INT;
+			return value / GTCEFE.RATIO_INT;
+		}
+
+		@Override
+		public long getInputAmperage() {
+			IEnergyStorage container = getAcceptionCap();
+
+			if (container == null) {
+				return 0L;
+			}
+
+			long voltage = getInputVoltage();
+
+			if (voltage == GTValues.V[GTValues.V.length]) {
+				return 1L;
+			}
+
+			for (int index = 0; index < GTValues.V.length; index++) {
+				if (GTValues.V[index] == voltage) {
+					long voltageNext = GTValues.V[index + 1] * GTCEFE.RATIO;
+
+					if (voltageNext > Integer.MAX_VALUE) {
+						voltageNext = Integer.MAX_VALUE;
+					}
+
+					int allowedInput = container.receiveEnergy((int) voltageNext, true);
+
+					if (allowedInput < voltage * GTCEFE.RATIO) {
+						return 1L;
+					}
+
+					allowedInput -= allowedInput % voltage * GTCEFE.RATIO;
+					return allowedInput / (voltage * GTCEFE.RATIO);
+				}
+			}
+
+			return 1L;
+		}
+
+		@Override
+		public long getInputVoltage() {
+			IEnergyStorage container = getStorageCap();
+
+			if (container == null) {
+				return 0L;
+			}
+
+			long grabMaxInput = container.receiveEnergy(Integer.MAX_VALUE, true);
+			grabMaxInput -= grabMaxInput % GTCEFE.RATIO;
+
+			if (grabMaxInput == 0) {
+				return 0L;
+			}
+
+			grabMaxInput /= GTCEFE.RATIO;
+
+			long value = GTValues.V[0];
+
+			if (grabMaxInput < value) {
+				return 0L;
+			}
+
+			for (long value2 : GTValues.V) {
+				if (value2 < grabMaxInput) {
+					break;
+				} else {
+					value = value2;
+				}
+			}
+
+			return value;
+		}
+
+		@Override
+		public boolean inputsEnergy(EnumFacing facing) {
+			int faceID = facing == null ? 6 : facing.getIndex();
+			IEnergyStorage container = facesRF[faceID];
+
+			if (container == null) {
+				container = upvalue.getCapability(CapabilityEnergy.ENERGY, facing);
+				facesRF[faceID] = container;
+			}
+
+			if (container == null) {
+				return false;
+			}
+
+			return container.canReceive();
+		}
+
+		@Override
+		public boolean outputsEnergy(EnumFacing arg0) {
+			// return container.canExtract();
+			// we just want to receive energy from ENet without hacks
+			// FE based blocks will push energy on it's own to us using EnergyContainerWrapper.
+			return false;
+		}
+	}
+
+	class EnergyContainerWrapper implements IEnergyStorage {
+		private final IEnergyContainer container;
+		private EnumFacing facing = null;
+
+		public EnergyContainerWrapper(IEnergyContainer container, EnumFacing facing) {
 			this.container = container;
+			this.facing = facing;
+		}
+
+		boolean isValid() {
+			return container != null && !(container instanceof GregicEnergyContainerWrapper);
 		}
 
 		private int maxSpeedIn() {
@@ -89,13 +421,31 @@ public class EnergyProvider implements ICapabilityProvider {
 			return (int) result;
 		}
 
+		private int voltageIn() {
+			long result = container.getInputVoltage() * GTCEFE.RATIO;
+
+			if (result > Integer.MAX_VALUE) {
+				return Integer.MAX_VALUE;
+			}
+
+			return (int) result;
+		}
+
+		private int voltageOut() {
+			long result = container.getOutputVoltage() * GTCEFE.RATIO;
+
+			if (result > Integer.MAX_VALUE) {
+				return Integer.MAX_VALUE;
+			}
+
+			return (int) result;
+		}
+
+		// eNet in gregtech is private
+		// im unable to workaround cable burning.
 		@Override
 		public int receiveEnergy(int maxReceive, boolean simulate) {
 			if (!canReceive()) {
-				return 0;
-			}
-
-			if (maxReceive < GTCEFE.RATIO * container.getInputVoltage()) {
 				return 0;
 			}
 
@@ -105,11 +455,16 @@ public class EnergyProvider implements ICapabilityProvider {
 				maxReceive = speed;
 			}
 
-			maxReceive = maxReceive - maxReceive % GTCEFE.RATIO_INT;
+			maxReceive -= maxReceive % GTCEFE.RATIO_INT;
+			maxReceive -= maxReceive % voltageIn();
+
+			if (maxReceive <= 0 || maxReceive < voltageIn()) {
+				return 0;
+			}
 
 			long missing = container.getEnergyCanBeInserted() * GTCEFE.RATIO;
 
-			if (missing <= 0L) {
+			if (missing <= 0L || missing < voltageIn()) {
 				return 0;
 			}
 
@@ -118,7 +473,8 @@ public class EnergyProvider implements ICapabilityProvider {
 			}
 
 			if (!simulate) {
-				container.changeEnergy(maxReceive / GTCEFE.RATIO);
+				int ampers = (int) container.acceptEnergyFromNetwork(this.facing, container.getInputVoltage(), maxReceive / (GTCEFE.RATIO * container.getInputVoltage()));
+				return ampers * voltageIn();
 			}
 
 			return maxReceive;
@@ -130,17 +486,18 @@ public class EnergyProvider implements ICapabilityProvider {
 				return 0;
 			}
 
-			if (maxExtract < GTCEFE.RATIO * container.getOutputVoltage()) {
-				return 0;
-			}
-
 			int speed = maxSpeedOut();
 
 			if (maxExtract > speed) {
 				maxExtract = speed;
 			}
 
-			maxExtract = maxExtract - maxExtract % GTCEFE.RATIO_INT;
+			maxExtract -= maxExtract % GTCEFE.RATIO_INT;
+			maxExtract -= maxExtract % voltageOut();
+
+			if (maxExtract <= 0) {
+				return 0;
+			}
 
 			long stored = container.getEnergyStored() * GTCEFE.RATIO;
 
@@ -152,8 +509,10 @@ public class EnergyProvider implements ICapabilityProvider {
 				maxExtract = (int) stored;
 			}
 
+			//GTCEFE.logger.info(maxExtract);
+
 			if (!simulate) {
-				container.changeEnergy(-maxExtract / GTCEFE.RATIO);
+				return (int) (container.removeEnergy(maxExtract / GTCEFE.RATIO) * GTCEFE.RATIO);
 			}
 
 			return maxExtract;
@@ -183,12 +542,16 @@ public class EnergyProvider implements ICapabilityProvider {
 
 		@Override
 		public boolean canExtract() {
-			return container.outputsEnergy(null);
+			if (container instanceof CableEnergyContainer) {
+				return false;
+			}
+
+			return container.outputsEnergy(this.facing);
 		}
 
 		@Override
 		public boolean canReceive() {
-			return container.inputsEnergy(null);
+			return container.inputsEnergy(this.facing);
 		}
 	}
 }
--- src/main/java/ru/dbotthepony/mc/gtcefe/GTCEFE.java
+++ src/main/java/ru/dbotthepony/mc/gtcefe/GTCEFE.java
@@ -20,15 +20,30 @@
 
 package ru.dbotthepony.mc.gtcefe;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.Logger;
+
 import gregtech.api.capability.GregtechCapabilities;
+import gregtech.api.capability.IEnergyContainer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(
 	modid = GTCEFE.MODID,
@@ -42,24 +57,22 @@ public class GTCEFE {
 	public static final long RATIO = 4;
 	public static final int RATIO_INT = 4;
 	public ResourceLocation resourceLocation;
+	public static Logger logger;
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register(this);
 		resourceLocation = new ResourceLocation(GTCEFE.MODID, "fecapability");
+		logger = event.getModLog();
 	}
 
 	@SubscribeEvent
 	public void attachTileCapability(AttachCapabilitiesEvent<TileEntity> event) {
-		//if (event.getObject().hasCapability(GregtechCapabilities.CAPABILITY_ENERGY_CONTAINER, null)) {
-			event.addCapability(resourceLocation, new EnergyProvider(event.getObject()));
-		//}
+		event.addCapability(resourceLocation, new EnergyProvider(event.getObject()));
 	}
 
 	@SubscribeEvent
 	public void attachItemCapability(AttachCapabilitiesEvent<ItemStack> event) {
-		//if (event.getObject().hasCapability(GregtechCapabilities.CAPABILITY_ENERGY_CONTAINER, null)) {
-			event.addCapability(resourceLocation, new EnergyProvider(event.getObject()));
-		//}
+		event.addCapability(resourceLocation, new EnergyProvider(event.getObject()));
 	}
 }
